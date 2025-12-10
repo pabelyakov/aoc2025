892a4a78128bce95247f94a76046aa8244515433
Petr Belyakov <pabelyak@mts.ru>
2025-12-10 12:44:27 +0300

    fix tests, added puzzle

--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,13 +1,11 @@
 from src.aoc2025.days.day01 import part01, part02
+from src.aoc2025.utils import input_util
+
+def get_input():
+    return input_util.read_input("01", "dev")
 
 def test_part_one():
-    """Тест для функции part_one"""
-    result = part01()
-    assert isinstance(result, int), "Результат должен быть целым числом"
-    assert result == 1, f"Ожидалось 42, получено {result}"
+    assert part01(get_input()) == 1, f"Ожидалось 42, получено {result}"
 
 def test_part_two():
-    """Тест для функции part_two"""
-    result = part02()
-    assert isinstance(result, int), "Результат должен быть целым числом"
-    assert result == 2, f"Ожидалось 100, получено {result}"
+    assert part02(get_input()) == 2, f"Ожидалось 100, получено {result}"
